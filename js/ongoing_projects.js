e06b69519dfa85a91a2f1b5e29cabce2589c169f
Sarah Wu <78280877+sarahhannahwu@users.noreply.github.com>
2026-08-12 11:37:48 -0700

    add url to ongoing project

--- js/ongoing_projects.js
+++ js/ongoing_projects.js
@@ -1,8 +1,12 @@
 /** Renders data/ongoing_projects.json (array of ongoing projects). */
 Site.load("./data/ongoing_projects.json", "projects-container", (container, projects) => {
-  projects.forEach(({ title, description }) => {
+  projects.forEach(({ title, description, url }) => {
     const item = Site.el("div", "item");
-    item.appendChild(Site.el("p", "item-title", title));
+    const heading = Site.el("p", "item-title");
+    if (url) heading.appendChild(Site.link(url, title));
+    else heading.textContent = title;
+    item.appendChild(heading);
+
     if (description) {
       item.appendChild(Site.el("p", "item-description", description));
     }
